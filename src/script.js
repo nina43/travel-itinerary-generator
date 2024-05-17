1f8ae8ca4396e99881b2c2cf9bf124fe1e480fe0
nina43 <161338284+nina43@users.noreply.github.com>
2024-05-17 12:09:54 +0100

    added the AI API integration

--- src/script.js
+++ src/script.js
@@ -1,12 +1,27 @@
-function generateItinerary(event) {
-  event.preventDefault();
-
+function displayItinerary(response) {
   new Typewriter("#itinerary", {
-    strings: "madrid",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateItinerary(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "1bf07a19dabb35aod3aet5596042cc7f";
+  let prompt = `user instructions: generate a travel itinerary about ${instructionsInput.value}`;
+  let context =
+    "you are travelling to anywhere in the world but do not know what to do. Your mission is to plan a itinerary in basic HTML with sites to see and foods to eat throughout your three day stay at that destination. Make sure to follow the user instructions. Sign the itinerary with 'SheCodes AI' in bold at the end of the itinerary";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating itinerary");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiUrl).then(displayItinerary);
 }
 
 let itineraryFormElement = document.querySelector("#itinerary-generator");
